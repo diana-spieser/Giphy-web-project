33cfa247ec9368f64c6f92950c5b7ee26161d06e
diana spieser <dianaspieser@gmail.com>
2023-07-21 13:08:24 +0200

    add handle url change functionality

--- src/events/navigation-events.js
+++ src/events/navigation-events.js
@@ -20,27 +20,35 @@ import { getFavorites } from '../data/favorites.js';
  */
 export const loadPage = async (page = '') => {
   switch (page) {
-  case HOME:
-    setActiveNav(HOME);
-    displaySearch();
-    clearSearchInput();
-    return renderHome();
-  case UPLOADS:
-    setActiveNav(UPLOADS);
-    hideSearch();
-    return renderUploads();
-  case FAVORITES:
-    setActiveNav(FAVORITES);
-    displaySearch();
-    clearSearchInput();
-    return renderFavorites();
-  case ABOUT:
-    setActiveNav(ABOUT);
-    hideSearch();
-    return renderAbout();
-
-  default:
-    return null;
+    case HOME:
+      setActiveNav(HOME);
+      displaySearch();
+      clearSearchInput();
+      await renderHome();
+      break;
+    case UPLOADS:
+      setActiveNav(UPLOADS);
+      hideSearch();
+      await renderUploads();
+      break;
+    case FAVORITES:
+      setActiveNav(FAVORITES);
+      displaySearch();
+      clearSearchInput();
+      await renderFavorites();
+      break;
+    case ABOUT:
+      setActiveNav(ABOUT);
+      hideSearch();
+      await renderAbout();
+      break;
+    default:
+      // For unknown routes, redirect to the home page
+      window.history.replaceState({}, '', '/' + HOME);
+      setActiveNav(HOME);
+      displaySearch();
+      clearSearchInput();
+      await renderHome();
   }
 };
 
@@ -69,9 +77,48 @@ export const renderFavorites = async () => {
 const renderAbout = () => {
   document.querySelector(CONTAINER_SELECTOR).innerHTML = toAboutView();
 };
+
 /**
  * Renders the uploads page.
  */
 export const renderUploads = async () => {
   q(CONTAINER_SELECTOR).innerHTML = await toUploadsView();
 };
+
+// Create a function to handle URL changes and trigger page rendering
+const handleUrlChange = () => {
+  const pageName = getPageNameFromUrl();
+  loadPage(pageName);
+};
+
+// Add a function to extract the page name from the URL
+const getPageNameFromUrl = () => {
+  const path = window.location.pathname;
+  const pageName = path.substring(1); // Remove the leading '/'
+  return pageName || HOME; // If no page name, default to the HOME page
+};
+
+// Call the handleUrlChange function when the page loads to render the initial page
+document.addEventListener('DOMContentLoaded', handleUrlChange);
+
+// Get all navigation links with the class "nav-link"
+const navLinks = document.querySelectorAll('.nav-link');
+
+// Add a click event listener to each navigation link
+navLinks.forEach((link) => {
+  link.addEventListener('click', (event) => {
+    event.preventDefault(); // Prevent default link behavior (e.g., page reload)
+
+    // Get the data-page attribute value
+    const page = link.dataset.page;
+
+    // Call a function to load/render the corresponding view based on the data-page value
+    loadPage(page);
+
+    // Update the URL to reflect the clicked link
+    history.pushState({}, '', `/${page}`);
+  });
+});
+
+// Call the handleUrlChange function whenever the URL changes (e.g., when the user clicks on the browser's back/forward buttons)
+window.addEventListener('popstate', handleUrlChange);
--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,7 @@ import CopyWebpackPlugin from 'copy-webpack-plugin';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
+
 export default {
   mode: 'development',
   entry: './src/index.js',
@@ -15,28 +16,7 @@ export default {
   },
   module: {
     rules: [
-      {
-        test: /\.html$/,
-        use: ['html-loader'],
-      },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
-      },
-      {
-        test: /\.(png|jpe?g|gif)$/i,
-        type: 'asset/resource',
-        generator: {
-          filename: 'images/[name][ext]',
-        },
-      },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/i,
-        type: 'asset/resource',
-        generator: {
-          filename: 'fonts/[name][ext]',
-        },
-      },
+      // Your existing rules...
     ],
   },
   plugins: [
@@ -54,5 +34,6 @@ export default {
       directory: path.resolve(__dirname, 'dist'),
     },
     open: true,
+    historyApiFallback: true,
   },
 };
